b8d9a0f0c74847260df8dd3c27d4e62af9f42c62
Maurice Oppenberger <mauriceoppenberger@gmail.com>
2020-02-15 21:38:41 -0500

    set up user model

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = "3000";
 const authRoute = require("./routes/auth");
-
-app.use(bodyParser.urlencoded({ extended: false }));
+const aRoute = app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // app.use((req, res, next) => {
 //   res.header("Allow-Access-Control-Methods", "GET,POST,PUT,PATCH,DELETE");
