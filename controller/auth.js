89fc3da5b86f4cce0bbd1d63627c87d7f48ed185
Maurice Oppenberger <mauriceoppenberger@gmail.com>
2020-02-17 11:23:16 -0500

    set up login route

--- controller/auth.js
+++ controller/auth.js
@@ -22,17 +22,39 @@ exports.signup = (req, res) => {
             email: req.body.email,
             password: hashedPassword
           });
-
           //Add user to database
           return user.save();
         })
         .catch(err => console.log(err));
-
-      ///set cookie!?!
     })
     .then(user => {
-      res.status(200).json({ message: "user created", user });
+      res.status(201).json({ message: "user created", user });
     })
 
     .catch(err => res.status(err.statusCode).json({ message: err.message }));
+};
+
+exports.login = (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        const error = new Error(
+          "No user found with this email, please use a valid email"
+        );
+        error.statusCode(401);
+        throw error;
+      }
+      return bcrypt
+        .compare(req.body.password, user.password)
+        .then(isMatch => {
+          if (!isMatch) {
+            const error = new Error("email or password invalid");
+            error.statusCode = 403;
+            throw error;
+          }
+          res.status(200).json({ message: user });
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
 };
